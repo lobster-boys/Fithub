651ed2cbc50d9568e6ded579de07a9e07871ce62
PepperShin <peep1996@naver.com>
2025-05-26 16:28:53 +0900

    feat: RESTapi 세팅

--- fithub/backend/config/settings.py
+++ fithub/backend/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",  # DRF
     # app 등록
     "users",
     "community",
