d7da93cbf37a0cc8ecb5e55908400b6637f164cc
9hakboiii <gyuhakarmy@gmail.com>
2025-05-27 16:35:11 +0900

    feat: dj-rest-auth api url 설계

--- fithub/backend/api/urls.py
+++ fithub/backend/api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 app_name = "api"
 
 urlpatterns = [
-    
+    # 로그인/회원가입 URL
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
