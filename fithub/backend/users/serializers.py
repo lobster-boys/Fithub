28256d33661f27bf5107fb029390ccd4a427a06c
9hakboiii <gyuhakarmy@gmail.com>
2025-05-28 15:44:35 +0900

    chore: 회원가입 POST 요청으로 first_name, last_name값이 없다면 null처리가 되도록 수정

--- fithub/backend/users/serializers.py
+++ fithub/backend/users/serializers.py
@@ -20,17 +20,23 @@ class CustomLoginSerializer(serializers.ModelSerializer):
 
 # 회원가입 필드 커스텀
 class CustomRegisterSerializer(RegisterSerializer):
-    first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=True)
+    first_name = serializers.CharField(required=False, allow_blank=True) 
+    last_name = serializers.CharField(required=False, allow_blank=True)
 
+    def validate(self, data):
+        if not data.get("first_name"):
+            data["first_name"] = None
+        if not data.get("last_name"):
+            data["last_name"] = None
+        return data
+    
     def get_cleaned_data(self):
         data = super().get_cleaned_data()
-        data['first_name'] = self.validated_data.get('first_name', '')
-        data['last_name'] = self.validated_data.get('last_name', '')
-
+        data["first_name"] = self.validated_data.get("first_name") or None
+        data["last_name"] = self.validated_data.get("last_name") or None
         return data
     
     def custom_signup(self, request, user):
-        user.first_name = self.validated_data.get('first_name')
-        user.last_name = self.validated_data.get('last_name')
+        user.first_name = self.validated_data.get("first_name") or None
+        user.last_name = self.validated_data.get("last_name") or None
         user.save()
