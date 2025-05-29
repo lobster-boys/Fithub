07816b83933aa7dc437065245a07395be3432ed1
9hakboiii <gyuhakarmy@gmail.com>
2025-05-29 13:50:32 +0900

    feat: kakao 소셜 로그인 path 생성

--- fithub/backend/api/urls.py
+++ fithub/backend/api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from .views import social_views
+
 
 app_name = "api"
 
@@ -6,4 +8,5 @@ urlpatterns = [
     # 로그인/회원가입 URL
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("dj-rest-auth/kakao/", social_views.KakaoLoginView.as_view(), name="kakao_login"),
 ]
