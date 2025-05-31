d10036186c65d3248ab945194b8a28286dadd6bf
9hakboiii <gyuhakarmy@gmail.com>
2025-05-31 14:55:33 +0900

    feat: 유저 프로필 path 등록

--- fithub/backend/api/urls.py
+++ fithub/backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import social_views
+from .views import social_views, profile_views
 
 
 app_name = "api"
@@ -9,4 +9,7 @@ urlpatterns = [
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
     path("dj-rest-auth/kakao/", social_views.KakaoLoginView.as_view(), name="kakao_login"),
+    # 유저 프로필 URL
+    path("dj-rest-auth/users/profile/", profile_views.UserProfileCreateView.as_view(), name="profile-create"),
+    path("dj-rest-auth/users/profile/<int:pk>/", profile_views.UserProfileDetail.as_view(), name="profile-detail"),
 ]
