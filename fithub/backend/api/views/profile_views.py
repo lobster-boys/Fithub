1c0cac217ca3f515e36dc4dbcac0838c74a3855f
9hakboiii <gyuhakarmy@gmail.com>
2025-06-01 21:08:58 +0900

    chore: serializer import 경로 수정

--- fithub/backend/api/views/profile_views.py
+++ fithub/backend/api/views/profile_views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from users.serializers import userprofile_serializers
+from ..serializers.users import userprofile_serializers
 from users.models import UserProfile
 from django.shortcuts import get_object_or_404
 from users.permissions import IsOwnerOrReadOnly
