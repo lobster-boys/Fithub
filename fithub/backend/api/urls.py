96d46f2b49825851f396c325e96eacc881a5d68f
9hakboiii <gyuhakarmy@gmail.com>
2025-06-03 19:30:50 +0900

    feat: 게시글 및 댓글 좋아요 URL 정의

--- fithub/backend/api/urls.py
+++ fithub/backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views.community import post_views, comment_views
+from .views.community import post_views, comment_views, post_like_views, comment_like_views
 from .views.social import social_views
 from .views.users import profile_views
 
@@ -19,5 +19,9 @@ urlpatterns = [
     path('dj-rest-auth/posts/<int:pk>/', post_views.UserPostDetail.as_view(), name='post-detail'),
     # 댓글 CRUD URL
     path('dj-rest-auth/posts/<int:post_id>/comments/', comment_views.UserCommentDetail.as_view(), name='comment-create'),
-    path('dj-rest-auth/posts/<int:post_id>/comments/<int:pk>/', comment_views.UserCommentDetail.as_view(), name='comment-detail')
+    path('dj-rest-auth/posts/<int:post_id>/comments/<int:pk>/', comment_views.UserCommentDetail.as_view(), name='comment-detail'),
+    # 게시글 좋아요 URL
+    path('dj-rest-auth/posts/<int:pk>/like/', post_like_views.PostLikeView.as_view(), name="post-like"),
+    # 댓글 좋아요 URL
+    path('dj-rest-auth/comments/<int:pk>/like/', comment_like_views.CommentLikeView.as_view(), name='comment-like')
 ]
