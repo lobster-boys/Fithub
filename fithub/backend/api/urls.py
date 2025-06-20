508ed4d52a7d40d4afe57dee5f8dfdea9a8a399a
9hakboiii <gyuhakarmy@gmail.com>
2025-06-20 22:47:29 +0900

    feat: DietLog url 생성

--- fithub/backend/api/urls.py
+++ fithub/backend/api/urls.py
@@ -3,7 +3,7 @@ from .views.community import post_views, comment_views, post_like_views, comment
 from .views.social import social_views
 from .views.users import profile_views
 from .views.audit import changelog_views
-from .views.diet import food_views, food_search_views, mealplan_views, recommend_views
+from .views.diet import food_views, food_search_views, mealplan_views, recommend_views, diet_log_views
 
 app_name = "api"
 
@@ -43,4 +43,9 @@ urlpatterns = [
     path('diet/mealplan/<int:pk>/', mealplan_views.MealPlanDetailView.as_view(), name='mealplan-detail'),
     # diet-recommend URL
     path('diet/recommend/', recommend_views.DietRecommendView.as_view(), name='diet-recommend'),
+    # diet-dietlog URL
+    path('diet/log/', diet_log_views.DietLogListView.as_view(), name='diet-log-list'),
+    path('diet/log/<int:pk>/', diet_log_views.DietLogDetailView.as_view(), name='diet-log-detail'),
+    path('diet/log/from-recommendation/', diet_log_views.DietLogFromRecommendationView.as_view(), name='diet-log-from-recommendation'),
+    path('diet/log/stats/', diet_log_views.DietLogStatsView.as_view(), name='diet-log-stats'),
 ]
